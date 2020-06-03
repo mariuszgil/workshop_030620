98671a58dfb8504241770cf7155b0dd1e74a9e3d
Mariusz Gil <mariusz@mariuszgil.pl>
2020-06-03 14:43:15 +0200

    changes :)

--- src/DocFlow/Domain/User.php
+++ src/DocFlow/Domain/User.php
@@ -4,5 +4,30 @@ namespace DocFlow\Domain;
 
 class User
 {
+    /**
+     * @var string
+     */
+    private $username;
 
+    /**
+     * User constructor.
+     * @param string $username
+     */
+    public function __construct(string $username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function equals(User $other): bool
+    {
+        return $this->username == $other->getUsername();
+    }
 }
