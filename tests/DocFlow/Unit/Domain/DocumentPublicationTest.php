f5ea5f6ebd40ae54ec72168696b1e48547a1bcf9
Mariusz Gil <mariusz@mariuszgil.pl>
2020-06-04 14:22:49 +0200

    more changes :)

---
+++ tests/DocFlow/Unit/Domain/DocumentPublicationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\DocFlow\Unit\Domain;
+
+use PHPUnit\Framework\TestCase;
+
+class DocumentPublicationTest extends TestCase
+{
+    public function testCanBePublishedAfterSuccessfulVerification()
+    {
+
+    }
+
+    public function testCantBePublishedWithoutVerification()
+    {
+        
+    }
+}
