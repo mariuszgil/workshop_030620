8a96b42220034af329affd52aef96c729bdc2d25
Mariusz Gil <mariusz@mariuszgil.pl>
2020-06-05 10:41:40 +0200

    changes :)

--- src/DocFlow/Infrastructure/VeriSignAdapter.php
+++ src/DocFlow/Infrastructure/VeriSignAdapter.php
@@ -9,7 +9,7 @@ use DocFlow\Domain\User;
 
 class VeriSignAdapter implements DocumentSigner
 {
-    public function sign(User $author, string $number): bool
+    public function sign(User $author, string $number): void
     {
         // TODO: Implement sign() method.
     }
